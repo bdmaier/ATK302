488f17af1ac765ebc6fa5e7840e785b17f989910
bdmaier <54369195+bdmaier@users.noreply.github.com>
2020-01-22 11:29:21 -0600

    Push2

---
+++ p5/Week2/trees/sketch.js
@@ -0,0 +1,125 @@
+function setup() {
+ createCanvas(720, 400);
+  // put setup code here
+}
+
+function draw() {
+  background('#1395F7');
+ noStroke();
+fill('green');
+rect(0, 320, 720, 80);
+ fill('yellow');
+ ellipse(700, 40, 160, 160);
+ fill(255);
+ // quad(300, 100, 340, 100,  340, 300, 300, 300,);
+ fill('#02350C');
+ arc(440, 175, 180, 180, PI, TWO_PI);
+
+ fill('#352602');
+ rect(420, 175, 40, 215);
+ fill('#02350C');
+ arc(300, 235, 140, 140, PI, TWO_PI);
+ fill('#352602');
+ rect(285, 235, 30, 140);
+ fill('#02350C');
+ arc(570, 280, 120, 120, PI, TWO_PI);
+ fill('#352602');
+ rect(560, 280, 20, 75);
+ fill('#02350C');
+ arc(136, 110, 210, 210, PI, TWO_PI);
+ fill('#352602');
+ rect(103, 110, 60, 300);
+fill('red');
+ ellipse(110, 90, 8, 8);
+ ellipse(88, 68, 8, 8);
+ ellipse(48, 73, 8, 8);
+ ellipse(122, 70, 8, 8);
+ ellipse(158, 53, 8, 8);
+ ellipse(79, 94, 8, 8);
+ ellipse(147, 94, 8, 8);
+ ellipse(181, 84, 8, 8);
+ ellipse(207, 62, 8, 8);
+ellipse(220, 90, 8, 8);
+ellipse(181, 33, 8, 8);
+ellipse(147, 18, 8, 8);
+ellipse(121, 34, 8, 8);
+ellipse(76, 42, 8, 8);
+ellipse(92, 21, 8, 8);
+ellipse(120, 14, 8, 8);
+fill('orange');
+ellipse(254, 192, 8, 8);
+ellipse(278, 174, 8, 8);
+ellipse(275, 190, 8, 8);
+ellipse(299, 172, 8, 8);
+ellipse(297, 190, 8, 8);
+ellipse(317, 174, 8, 8);
+ellipse(332, 178, 8, 8);
+ellipse(316, 189, 8, 8);
+ellipse(243, 206, 8, 8);
+ellipse(239, 222, 8, 8);
+ellipse(265, 208, 8, 8);
+ellipse(284, 202, 8, 8);
+ellipse(304, 204, 8, 8);
+ellipse(344, 188, 8, 8);
+ellipse(332, 194, 8, 8);
+ellipse(320, 204, 8, 8);
+ellipse(353, 199, 8, 8);
+ellipse(360, 214, 8, 8);
+ellipse(338, 210, 8, 8);
+ellipse(256, 220, 8, 8);
+ellipse(276, 217, 8, 8);
+ellipse(295, 218, 8, 8);
+ellipse(312, 220, 8, 8);
+ellipse(329, 225, 8, 8);
+ellipse(345, 226, 8, 8);
+ellipse(284, 228, 8, 8);
+ellipse(269, 228, 8, 8);
+
+fill('purple');
+ellipse(364, 161, 12, 12);
+ellipse(370, 133, 12, 12);
+ellipse(393, 108, 12, 12);
+ellipse(389, 148, 12, 12);
+ellipse(403, 129, 12, 12);
+ellipse(416, 99, 12, 12);
+ellipse(440, 94, 12, 12);
+ellipse(428, 116, 12, 12);
+ellipse(409, 160, 12, 12);
+ellipse(422, 136, 12, 12);
+ellipse(454, 114, 12, 12);
+ellipse(444, 130, 12, 12);
+ellipse(458, 94, 12, 12);
+ellipse(478, 103, 12, 12);
+ellipse(431, 153, 12, 12);
+ellipse(468, 129, 12, 12);
+ellipse(455, 146, 12, 12);
+ellipse(488, 123, 12, 12);
+ellipse(508, 135, 12, 12);
+ellipse(484, 142, 12, 12);
+ellipse(500, 157, 12, 12);
+ellipse(471, 157, 12, 12);
+ellipse(444, 166, 12, 12);
+ellipse(518, 152, 12, 12);
+
+// https://youtu.be/deer3UmmQMg
+
+ fill(0);
+ textSize(30);
+  text(mouseX + ", " + mouseY, 50, 50);
+
+ // fill(204);
+ // triangle(18, 18, 18, 360, 81, 360);
+ //
+
+ //
+ // fill(204);
+ // quad(189, 18, 216, 18, 216, 360, 144, 360);
+ //
+
+ //
+ // fill(204);
+ // triangle(288, 18, 351, 360, 288, 360);
+ //
+
+ //  // put drawing code here
+}
