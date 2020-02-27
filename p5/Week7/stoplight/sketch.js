37f3392a9308b341efe4e3d83698ddcbe31abefd
bdmaier <54369195+bdmaier@users.noreply.github.com>
2020-02-27 11:05:02 -0600

    Stoplight

---
+++ p5/Week7/stoplight/sketch.js
@@ -0,0 +1,68 @@
+var myState = 0;
+var timer = 0;
+
+function setup() {
+  createCanvas(800, 800);
+  rectMode(CENTER);
+  ellipseMode(CENTER);
+  background('turquoise');
+  // put setup code here
+}
+
+function draw() {
+fill('#E4E422');
+  rect(width/2, height/2, 150, 400);
+
+  //put drawing code Here
+  //yellow rectangle
+  switch(myState)
+  {
+
+
+  case 0:
+  fill('green');
+ellipse(width/2, 500, 80, 80);
+fill('grey');
+  ellipse(width/2, 382, 80, 80);
+  ellipse(width/2, height/3, 80, 80);
+
+    timer++;
+     if (timer > 150){
+       myState = 1;
+       timer = 0;
+     }
+   //red light
+
+  break;
+
+  case 1: //green light
+fill('grey');
+  ellipse(width/2, 500, 80, 80);
+fill('yellow');
+    ellipse(width/2, 382, 80, 80);
+fill('grey');
+    ellipse(width/2, height/3, 80, 80);
+
+    timer++;
+     if (timer > 150){
+       myState = 2;
+       timer = 0;
+     }
+break;
+
+  case 2: //yellow light
+fill('grey');
+  ellipse(width/2, 500, 80, 80);
+    ellipse(width/2, 382, 80, 80);
+fill('red');
+    ellipse(width/2, height/3, 80, 80);
+  timer++;
+   if (timer > 150){
+     myState = 0;
+     timer = 0;
+   }
+
+  break;
+}
+
+}
