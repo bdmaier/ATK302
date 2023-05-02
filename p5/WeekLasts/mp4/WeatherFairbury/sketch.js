89cf10aae0742cfeb717bcf0e5c337aca178ecb9
bdmaier <54369195+bdmaier@users.noreply.github.com>
2023-05-01 21:17:33 -0500

    Update sketch.js

--- p5/WeekLasts/mp4/WeatherFairbury/sketch.js
+++ p5/WeekLasts/mp4/WeatherFairbury/sketch.js
@@ -120,7 +120,8 @@ function draw() {
 
       textStyle(BOLD);
       text("Temperature: ")
-      textStyle(NORMAL) text(+ weather.main.temp + "°F("+ sTemp.toFixed(2) + "°C)\n" + "High of: " + weather.main.temp_max + "°F(" + maxTemp.toFixed(2) + "°C)" + "  Low of: " + weather.main.temp_min + "°(" + minTemp.toFixed(2) + "°C)", 30, 250);
+      textStyle(NORMAL) 
+      text(+ weather.main.temp + "°F("+ sTemp.toFixed(2) + "°C)\n" + "High of: " + weather.main.temp_max + "°F(" + maxTemp.toFixed(2) + "°C)" + "  Low of: " + weather.main.temp_min + "°(" + minTemp.toFixed(2) + "°C)", 30, 250);
       text("FEELS like: " + weather.main.feels_like + "°F(" + feelsLike.toFixed(2) + "°C)", 30, 300);
       text("Humidity: " + weather.main.humidity + "%.", 30, 330);
       text("Wind speed: " + weather.wind.speed + "mph, with gusts of up to " + weather.wind.gust + "mph.", 30, 360);
