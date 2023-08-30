fe0d84ece7c273d1730740eddf1408307f573da4
bdmaier <54369195+bdmaier@users.noreply.github.com>
2023-08-29 23:35:43 -0500

    Update sketch.js

--- p5/oryx/sketch.js
+++ p5/oryx/sketch.js
@@ -5,7 +5,7 @@ var oryx ;
 function setup() {
   // put setup code here
 createCanvas(800, 800);
-oryx = loadImage("assets/oryx.jpeg");
+oryx = loadImage("assets/oryx.jpg");
 //chicken2 = loadImage("assets/chicken2.jpg");
 //chicken3 = loadImage("assets/chicken3.jpg");
 }
