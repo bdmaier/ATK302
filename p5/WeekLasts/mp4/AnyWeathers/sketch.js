8bde8ca34ba57a52ccb53a0661aa3cf098522394
bdmaier <54369195+bdmaier@users.noreply.github.com>
2025-04-09 00:44:50 -0500

    Update sketch.js

--- p5/WeekLasts/mp4/AnyWeathers/sketch.js
+++ p5/WeekLasts/mp4/AnyWeathers/sketch.js
@@ -135,12 +135,12 @@ function draw() {
       text("Sunrise time: " + convertTime(srtime) + ".", 30, 400);
       text("Sunset time: " + convertTime2(sstime) + ".", 30, 425);
       text("Weather: " + weather.weather[0].description, 30, 450);
-      text("Pressure: " + press.toFixed(2) + "psi/" + pressmerc.toFixed(2) + "inHg/" + pressure + "mb (hPa)/" + pressatm.toFixed(4) + "atm", 30, 475);
-      text("Visibility: " + visibility.toFixed(2) + "mi", 30, 550);
+      text("Pressure: " + press.toFixed(2) + "psi/" + pressmerc.toFixed(2) + "inHg/" + pressure + "mb(hPa)/" + pressatm.toFixed(4) + "atm", 30, 525);
+      text("Visibility: " + visibility.toFixed(2) + "mi", 30, 500);
       text("Coordinates of " + weather.name + ": " + weather.coord.lon + " W LONG, " + weather.coord.lat + " N LAT", 30, 575);
       text("ID of " + weather.name + ": " + weather.sys.id, 30, 600);
-      text("Pressure (Grnd Lvl): " + pressGrnd.toFixed(2) + "psi/" + pressGrndMerc.toFixed(2) + "inHg/" + grndLevPressure + "mb (hPa)/" + pressGrndAtm.toFixed(4) + "atm", 30, 500);
-      text("Cloudiness: " + cloudiness + "%", 30, 525);
+      text("Pressure (Grnd Lvl): " + pressGrnd.toFixed(2) + "psi/" + pressGrndMerc.toFixed(2) + "inHg/" + grndLevPressure + "mb(hPa)/" + pressGrndAtm.toFixed(4) + "atm", 30, 550);
+      text("Cloudiness: " + cloudiness + "%", 30, 475);
       //text('link1', 30, 570);
       text(formattedDate, 30, 690);
       fill('white');
